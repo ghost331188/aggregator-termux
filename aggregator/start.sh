2cfc81e7c1ae45998c1d8a0c30bf7a8de309aba0
ghost331188 <102396848+ghost331188@users.noreply.github.com>
2024-06-25 12:16:53 +0800

    Update start.sh

--- aggregator/start.sh
+++ aggregator/start.sh
@@ -6,7 +6,6 @@ pkg update -y && pkg upgrade -y
 # 安装Python、pip和虚拟环境包
 pkg install python -y
 pkg install python-pip -y
-pkg install python-virtualenv -y
 
 # 更换pip源为国外源（例如使用清华大学的源）
 pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
@@ -14,10 +13,6 @@ pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 
 # 进入项目目录
 cd aggregator-termux
-
-# 创建并激活Python虚拟环境
-python3 -m venv venv
-source venv/bin/activate
 
 # 安装依赖
 pip install pyYAML tqdm
